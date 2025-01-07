f5100c7ee9c22a2857d938c65755958deb7a5108
aquinq <aquinq@hotmail.fr>
2025-01-07 18:50:56 +0100

    rename day folders

--- index.ts
+++ index.ts
@@ -3,8 +3,9 @@ import { readData } from './utils';
 const [day, part] = [12, 2];
 
 const main = async () => {
-  const { default: run } = await import(`./src/day-${day}/${part}.js`);
-  const data = readData(`./src/day-${day}/data.txt`);
+  const d = day.toString().padStart(2, '0');
+  const { default: run } = await import(`./src/day-${d}/${part}.js`);
+  const data = readData(`./src/day-${d}/data.txt`);
   const result = run(data);
   console.log(result);
 };
